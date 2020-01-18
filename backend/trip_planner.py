2fa6025255327ea866927c4f8dc6316ca391cb7c
Joseph-Acevedo <joseph.acevedo@outlook.com>
2020-01-18 13:45:38 -0700

    Finished trip planning. Still needs ordering and adding API key to icnrease requests in TTC

--- backend/trip_planner.py
+++ backend/trip_planner.py
@@ -7,8 +7,8 @@ from copy import deepcopy
 
 def find_carbon_paths(source, destination, car_mpg, max_cost, max_time, depart_time):
     trips = start_ground_trips(source, destination, car_mpg)
-    print(trips[0], " last city: ", trips[0].get_last_city())
-    find_flights(trips, destination, max_cost, max_time, depart_time)
+    updated_trips = find_flights(trips, destination, max_cost, max_time, depart_time)
+    finish_trips(updated_trips, destination, max_cost, max_time, car_mpg)
 
 """
 
@@ -53,7 +53,6 @@ def start_ground_trips(source, destination, car_mpg):
     
 def find_flights(curr_trips, destination, max_cost, max_time, depart_time):
     end_cities = nearby_airports(destination)
-    print(curr_trips)
     updated_trips = []
     for trip in curr_trips:
         if trip.money_cost >= max_cost or trip.time_cost >= max_time:
@@ -73,8 +72,45 @@ def find_flights(curr_trips, destination, max_cost, max_time, depart_time):
                     updated_trips.append(curr_trip)
     return updated_trips
 
-def finish_trips(curr_trips, destination, max_cost, max_time):
+def finish_trips(curr_trips, destination, max_cost, max_time, car_mpg):
+    finished_trips = []
     for trip in curr_trips:
-        pass
+        prev_city = trip.get_last_city()
+        if prev_city == destination:
+            finished_trips.append(trip)
+            continue
+        ground_paths = total_ground_cost(prev_city, destination, car_mpg)
+        # By car
+        car_costs = ground_paths[0]
+        car_trip = deepcopy(trip)
+        car_trip_step = TripStep(destination, TripStep.CAR, car_costs[0], car_costs[1], car_costs[2])
+        car_trip.cities.append(car_trip_step)
+        car_trip.carbon_cost += float(car_costs[0])
+        car_trip.money_cost += car_costs[1]
+        car_trip.time_cost += car_costs[2]
+        if car_trip.money_cost <= max_cost and car_trip.time_cost <= max_time:
+            finished_trips.append(car_trip)
+        # By bus
+        bus_costs = ground_paths[1]
+        bus_trip = deepcopy(trip)
+        bus_trip_step = TripStep(destination, TripStep.BUS, bus_costs[0], bus_costs[1], bus_costs[2])
+        bus_trip.cities.append(bus_trip_step)
+        bus_trip.carbon_cost += float(bus_costs[0])
+        bus_trip.money_cost += bus_costs[1]
+        bus_trip.time_cost += bus_costs[2]
+        if bus_trip.money_cost <= max_cost and bus_trip.time_cost <= max_time:
+            finished_trips.append(bus_trip)
+        # By train
+        train_costs = ground_paths[2]
+        train_trip = deepcopy(trip)
+        train_trip_step = TripStep(destination, TripStep.TRAIN, train_costs[0], train_costs[1], train_costs[2])
+        train_trip.cities.append(train_trip_step)
+        train_trip.carbon_cost += float(train_costs[0])
+        train_trip.money_cost += train_costs[1]
+        train_trip.time_cost += train_costs[2]
+        if train_trip.money_cost <= max_cost and train_trip.time_cost <= max_time:
+            finished_trips.append(train_trip)
+    print(finished_trips)
+    
 
 find_carbon_paths("Tucson", "Seattle", 35, 10000, 100, "2020-01-15")
