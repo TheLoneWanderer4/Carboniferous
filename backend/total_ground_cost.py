2b8563437d25284dd14a63b73c3e539db95e831c
Joseph-Acevedo <joseph.acevedo@outlook.com>
2020-01-18 01:40:20 -0700

    Completed calculations of costs for ground travel from one city to another

--- backend/total_ground_cost.py
+++ backend/total_ground_cost.py
@@ -1,6 +1,7 @@
 """
 Calculates the total carbon cost (C), monetary cost ($), and time cost (t) 
-as a triple for the trip from a source city to a destination city.
+as a triple for the trip from a source city to a destination city for all ground
+travel options.
 """
 import json
 import requests
@@ -8,13 +9,7 @@ import googlemaps as maps
 from datetime import datetime as dt
 
 
-# Constants used for calling the Trip to Carbon API
-CAR = "petrolCar"
-BUS = "bus"
-TRAIN = "transitRail"
-LIST_FUEL_TYPES_USED = [CAR, BUS, TRAIN]
-
-# The URL used to request the carbon costs from the Trip to Carbon API
+# Constants used for querying Google Maps and Trip to Carbon APIs
 MAPS_BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
 CARBON_BASE_URL = 'https://api.triptocarbon.xyz/v1/footprint?country=usa&activity='
 CARBON_CAR_URL_EXT = '&fuelType=motorGasoline&mode=petrolCar'
@@ -25,8 +20,10 @@ CARBON_TRAIN_URL_EXT = '&mode=transitRail'
 API_KEY = 'AIzaSyCK4gZqTHsd4Fi7_enR4aaDuyFGwmi3Je4' 
 
 # Constants for calculating costs of travel
-PETROL_PRICE_PER_GAL = 2.83
+PETROL_PRICE_PER_GAL_USD = 2.83
 BUS_PRICE_PER_MILE_USD = 0.10
+TRAIN_PRICE_PER_MILE_USD = 0.50
+
 
 
 """
@@ -39,37 +36,47 @@ city_b: The full name of the destination city
 mpg: a decimal number representing the miles per gallon that the user specifies
 
 Return:
-a list of triples, one for each of the ground
-options: car, bus, and train. The triple is in the order (C, $, t)
+a list of triples, one for each of the ground options: car, bus, and train in 
+that order. The triple is in the order (C, $, t)
 """
 def total_ground_cost(city_a, city_b, mpg):
     list_of_costs = []
-    # Request Car cost
+    # Car Costs
     miles_by_car, hours_by_car = get_distance_and_time_by_car(city_a, city_b)
-    gallons_used = str(miles_by_car // mpg)
-    response = requests.get(CARBON_BASE_URL + gallons_used + '&activityType=fuel' + CARBON_CAR_URL_EXT)
+    gallons_used = miles_by_car // mpg
+    response = requests.get(CARBON_BASE_URL + str(gallons_used) + '&activityType=fuel' + CARBON_CAR_URL_EXT)
     list_of_costs.append( (response.json()['carbonFootprint'], \
-            gallons_used * PETROL_PRICE_PER_GAL, \
+            gallons_used * PETROL_PRICE_PER_GAL_USD, \
             hours_by_car) )
 
-    # Request Bus cost
+    # Bus Costs
     miles_by_bus, hours_by_bus = get_distance_and_time_by_bus(city_a, city_b)
-    response = requests.get(CARBON_BASE_URL + miles_by_bus + '&activityType=miles' + CARBON_BUS_URL_EXT)
+    response = requests.get(CARBON_BASE_URL + str(miles_by_bus) + '&activityType=miles' + CARBON_BUS_URL_EXT)
     list_of_costs.append( (response.json()['carbonFootprint'], \
             miles_by_bus * BUS_PRICE_PER_MILE_USD, \
             hours_by_bus) )
 
-    # Request Train cost
+    # Train Costs
     miles_by_train, hours_by_train = get_distance_and_time_by_train(city_a, city_b)
-    response = requests.get(CARBON_BASE_URL + miles_by_train + '&activityType=miles' + CARBON_TRAIN_URL_EXT)
+    response = requests.get(CARBON_BASE_URL + str(miles_by_train) + '&activityType=miles' + CARBON_TRAIN_URL_EXT)
     list_of_costs.append( (response.json()['carbonFootprint'], \
-            gallons_used * PETROL_PRICE_PER_GAL, \
-            hours_by_car) )
+            miles_by_train * TRAIN_PRICE_PER_MILE_USD, \
+            hours_by_train) )
+    
+    return list_of_costs
 
 
 """
 Returns the number of miles to drive by car from city_a to city_b as well as a decimal
 for the number of hours to get there by using the Google Maps Distance Matrix API
+
+Parameters: 
+city_a: The full name of the source city
+city_b: The full name of the destination city
+
+Return:
+a tuple that gives the number of miles from city_a to city_b by driving a car
+and the time in number of hours to drive that distance
 """
 def get_distance_and_time_by_car(city_a, city_b):
     response = requests.get(MAPS_BASE_URL + \
@@ -85,6 +92,19 @@ def get_distance_and_time_by_car(city_a, city_b):
     except KeyError:
         return (-1, -1)
 
+
+"""
+Returns the number of miles to take a bus from city_a to city_b as well as a decimal
+for the number of hours to get there by using the Google Maps Distance Matrix API
+
+Parameters: 
+city_a: The full name of the source city
+city_b: The full name of the destination city
+
+Return:
+a tuple that gives the number of miles from city_a to city_b by taking a car
+and the time in number of hours to travel that distance
+"""
 def get_distance_and_time_by_bus(city_a, city_b):
     response = requests.get(MAPS_BASE_URL + \
         '&mode=transit&transit_mode=' + 'bus' + \
@@ -99,6 +119,19 @@ def get_distance_and_time_by_bus(city_a, city_b):
     except KeyError:
         return (-1, -1)
 
+
+"""
+Returns the number of miles to take a train from city_a to city_b as well as a decimal
+for the number of hours to get there by using the Google Maps Distance Matrix API
+
+Parameters: 
+city_a: The full name of the source city
+city_b: The full name of the destination city
+
+Return:
+a tuple that gives the number of miles from city_a to city_b by taking a train
+and the time in number of hours to travel that distance
+"""
 def get_distance_and_time_by_train(city_a, city_b):
     response = requests.get(MAPS_BASE_URL + \
         '&mode=transit&transit_mode=' + 'train' + \
@@ -112,6 +145,3 @@ def get_distance_and_time_by_train(city_a, city_b):
         return (int(num_miles), (time_secs / 3600) )
     except KeyError:
         return (-1, -1)
-
-print(get_distance_and_time_by_car('Tucson', 'Tempe'))
-    
