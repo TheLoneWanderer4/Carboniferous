d8baf9a8e0734c91b0c7bf0184f1573ca5fc77ed
Joseph-Acevedo <joseph.acevedo@outlook.com>
2020-01-18 08:57:40 -0700

    Added Trip object file, might still be modified

--- backend/trip.py
+++ backend/trip.py
@@ -9,8 +9,8 @@ class Trip:
     total_costs is the triple tuple of costs (carbon, mooney, time)
     city is a tuple with the airport name and the mode of travel there
     """
-    def __init__(self, city, total_costs):
-        self.cities = [city]
+    def __init__(self, cities, total_costs):
+        self.cities = cities
         self.carbon_cost = total_costs[0]
         self.money_cost = total_costs[1]
         self.time_cost = total_costs[2]
