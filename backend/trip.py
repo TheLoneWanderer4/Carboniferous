adb2f77a9420f9f95a810964283d70e08ee3f86f
Joseph-Acevedo <joseph.acevedo@outlook.com>
2020-01-18 10:01:51 -0700

    Beginning steps of creating paths for trips

--- backend/trip.py
+++ backend/trip.py
@@ -4,11 +4,20 @@ class Trip:
     total_costs is the triple tuple of costs (carbon, money, time)
     cities is a list of TripSteps
     """
-    def __init__(self, cities, total_costs):
-        self.cities = cities
-        self.carbon_cost = total_costs[0]
-        self.money_cost = total_costs[1]
-        self.time_cost = total_costs[2]
+
+    def __init__(self, start):
+        self.cities = []
+        self.cities.append(TripStep(start, None, 0.0, 0.0, 0.0))
+        self.cities = []
+        self.carbon_cost = 0.0
+        self.money_cost = 0.0
+        self.time_cost = 0.0
+
+    def __repr__(self):
+        curr = "{" + str(self.carbon_cost) + "," + str(self.money_cost) + "," + str(self.time_cost) + "} "
+        for city in self.cities:
+            curr += (city.current_city + '(' + city.transport_mode + ')' + '->')
+        return curr + 'ENDTRIP\n'
 
 class TripStep:
     # Modes of transportation allowed
@@ -22,4 +31,14 @@ class TripStep:
     transport_mode = ""
     carbon_cost = 0.0
     dollar_cost = 0.0
-    time_Cost = 0.0
+    time_cost = 0.0
+
+    def __init__(self, city, mode, carbon, money, time):
+        self.current_city = city
+        if mode == None:
+            self.transport_mode = ""
+        else:
+            self.transport_mode = mode
+        self.carbon_cost = carbon
+        self.dollar_cost = money
+        self.time_cost = time
--- backend/trip_planner.py
+++ backend/trip_planner.py
@@ -1,6 +1,7 @@
 from nearby_cities import nearby_airports
 from total_ground_cost import total_ground_cost
-from trip import Trip
+from total_air_cost import total_air_cost
+from trip import Trip, TripStep
 
 
 def find_carbon_paths(source, destination, car_mpg):
@@ -8,9 +9,26 @@ def find_carbon_paths(source, destination, car_mpg):
 
 def start_trips(source, destination, car_mpg):
     src_airports = nearby_airports(source)
+    print("Airports: ", src_airports)
     trips = []
     for airport in src_airports:
         ground_paths = total_ground_cost(source, airport, car_mpg)
-        trips.append((airport, Trip.CAR), ground_paths[0])
-        trips.append((airport, Trip.BUS), ground_paths[1])
-        trips.append((airport, Trip.TRAIN), ground_paths[2])
+
+        car_trip = Trip(source)
+        car_costs = ground_paths[0]
+        car_trip.cities.append(TripStep(airport, TripStep.CAR, car_costs[0], car_costs[1], car_costs[2]))
+        trips.append(car_trip)
+        
+        bus_trip = Trip(source)
+        bus_costs = ground_paths[1]
+        bus_trip.cities.append(TripStep(airport, TripStep.BUS, bus_costs[0], bus_costs[1], bus_costs[2]))
+        trips.append(bus_trip)
+
+        train_trip = Trip(source)
+        train_costs = ground_paths[1]
+        train_trip.cities.append(TripStep(airport, TripStep.TRAIN, train_costs[0], train_costs[1], train_costs[2]))
+        trips.append(train_trip)
+        
+    
+
+find_carbon_paths("Tucson", "Denver", 35)
