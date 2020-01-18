fba92c35a2c5a817c6ba578bb5022fc81961c2bd
Amanda Bertsch <abertsch72@gmail.com>
2020-01-18 12:37:13 -0700

    reads api key file

--- backend/api_management.py
+++ backend/api_management.py
@@ -1,4 +1,24 @@
+import json
+default_key_loc = "api_keys.json"
 
 class APIKeys:
-    def __init__(self, key_loc):
-        pass
+    google_key = ""
+    lutf_main_key = ""
+    lutf_private_key = ""
+    def __init__(self, key_loc = default_key_loc):
+        keys = json.load(open(key_loc))
+        self.google_key = keys["GoogleMaps"]
+        self.lutf_main_key = keys["LuftMain"]
+        self.lutf_private_key = keys["LuftPrivate"]
+
+    @staticmethod
+    def get_google_key(self):
+        return self.google_key
+
+    @staticmethod
+    def get_lutf_main_key(self):
+        return self.lutf_main_key
+
+    @staticmethod
+    def get_lutf_private_key(self):
+        return self.lutf_private_key
