77894451c60aa9648aeced8a6b84689cb2744f73
Amanda Bertsch <abertsch72@gmail.com>
2020-01-18 10:15:17 -0700

    making the object a list

--- backend/main.py
+++ backend/main.py
@@ -8,16 +8,16 @@ def main(input_json):
 
     # find_carbon_paths will return a list of path objects to return as a json
     # files to the front end.
-    return_dict = {}
+    return_list = []
     final_list = find_carbon_paths(start_city,end_city)
     for i in range(5):
         print(len(final_list))
         if(len(final_list) <= i):
             break
-        return_dict[i] = final_list[i].make_dict()
+        return_list.append(final_list[i].make_dict())
 
-    print(return_dict)
-    return json.dumps(return_dict)
+    print(return_list)
+    return json.dumps(return_list)
 
 """
 trip: {
