f0104d8672ea2e3371b096eff074ac663b44d4c4
Amanda Bertsch <abertsch72@gmail.com>
2020-01-18 09:26:10 -0700

    trips convert into json for return

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,34 @@
 import json
 from trip_planner import find_carbon_paths
-'''
-More probably needs to be added to this
-'''
+
 def main(input_json):
     input_data = input_json.json()
     start_city = input_data["start"]
     end_city = input_data["end"]
 
-    # DO STUFF HERE
-
     # find_carbon_paths will return a list of path objects to return as a json
     # files to the front end.
     return_dict = {}
     final_list = find_carbon_paths(start_city,end_city)
-    for i in range(0,5):
-        return_dict["Trip "+ (i+1)] = final_list[i].__dict__
+    for i in range(5):
+        return_dict[i] = final_list[i].make_dict()
+
     return json.dumps(return_dict)
+
+
+"""
+trip: {
+    steps: [
+        {
+            current_city: "",
+            transport: "" (will be None, train, bus, car, plane)
+            carbon_cost:
+            dollar_cost:
+            time_cost:
+        }
+    ]
+    total_carbon:
+    total_dollars:
+    total_time: 
+}
+"""
--- backend/trip.py
+++ backend/trip.py
@@ -10,16 +10,46 @@ class Trip:
         self.money_cost = total_costs[1]
         self.time_cost = total_costs[2]
 
-class TripStep:
-    # Modes of transportation allowed
-    PLANE = "plane"
-    CAR = "car"
-    BUS = "bus"
-    TRAIN = "train"
+    """
+    builds trip into a dictionary for json object creation
+    """
+    def make_dict(self):
+        dict = {}
+        step_list = []
+        total_carbon = 0
+        total_dollars = 0
+        total_time = 0
+        for step in self.cities:
+            step_dict = {}
+            step_dict["current_city"] = step.current_city
+            step_dict["transport"] = str(step.transport_mode)
+            step_dict["carbon_cost"] = step.carbon_cost
+            step_dict["dollar_cost"] = step.dollar_cost
+            step_dict["time_cost"] = step.time_cost
+            step_list.append(step_dict)
 
-    # Fields for a TripStep
-    current_city = ""
-    transport_mode = ""
-    carbon_cost = 0.0
-    dollar_cost = 0.0
-    time_Cost = 0.0
+            total_carbon += step.carbon_cost
+            total_dollars += step.dollar_cost
+            total_time += step.time_cost
+
+
+        dict["steps"] = step_list
+        dict["total_carbon"] = total_carbon
+        dict["total_dollars"] = total_dollars
+        dict["total_time"] = total_time
+
+        return dict
+
+class TripStep:
+# Modes of transportation allowed
+PLANE = "plane"
+CAR = "car"
+BUS = "bus"
+TRAIN = "train"
+
+# Fields for a TripStep
+current_city = ""
+transport_mode = ""
+carbon_cost = 0.0
+dollar_cost = 0.0
+time_cost = 0.0
