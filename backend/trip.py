7d76996d4049d3e6e8355545c6b0dd56f2ea166b
Joseph-Acevedo <joseph.acevedo@outlook.com>
2020-01-18 09:11:45 -0700

    Updated Trip.py with Trip and TripStep objects

--- backend/trip.py
+++ backend/trip.py
@@ -1,13 +1,8 @@
 class Trip:
 
-    PLANE = "plane"
-    CAR = "car"
-    BUS = "bus"
-    TRAIN = "train"
-
     """
-    total_costs is the triple tuple of costs (carbon, mooney, time)
-    city is a tuple with the airport name and the mode of travel there
+    total_costs is the triple tuple of costs (carbon, money, time)
+    cities is a list of TripSteps
     """
     def __init__(self, cities, total_costs):
         self.cities = cities
@@ -15,4 +10,16 @@ class Trip:
         self.money_cost = total_costs[1]
         self.time_cost = total_costs[2]
 
-    
+class TripStep:
+    # Modes of transportation allowed
+    PLANE = "plane"
+    CAR = "car"
+    BUS = "bus"
+    TRAIN = "train"
+
+    # Fields for a TripStep
+    current_city = ""
+    transport_mode = ""
+    carbon_cost = 0.0
+    dollar_cost = 0.0
+    time_Cost = 0.0
