5f14e3c198e9ed01e24ae97cf73e564d7dc26d25
TheLoneWanderer4 <aminsennour@gmail.com>
2020-01-18 22:35:35 -0700

    Update, integration fixed main

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,17 @@ def main(args):
         input_data["start"],input_data["end"],int(input_data["modes"]["car"]["mpg"])
     max_cost,max_time,depart_date = \
         int(input_data["maxPrice"]),int(input_data["maxTime"]),input_data["Date"]
+    modes = [True,True,True,True]
+    modes[0] = input_data["modes"]["car"]["allowed"]
+    modes[1] = input_data["modes"]["bus"]["allowed"]
+    modes[2] = input_data["modes"]["train"]["allowed"]
+    modes[3] = input_data["modes"]["plane"]["allowed"]
     # find_carbon_paths will return a list of path objects to return as a json
     # files to the front end.
     return_list = []
 
     final_list = find_carbon_paths(start_city,end_city,car_mpg,max_cost,\
-        max_time,depart_date, key_vault)
+        max_time,depart_date, modes, key_vault)
 
     # Serializing data to send back across
     for i in range(5):
