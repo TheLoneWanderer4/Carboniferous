17d79cb26d33ae33240165bcb3315aac78562783
TheLoneWanderer4 <aminsennour@gmail.com>
2020-01-18 23:06:25 -0700

    Update

--- client/src/components/TripTableRow.js
+++ client/src/components/TripTableRow.js
@@ -4,9 +4,9 @@ export default props => {
   return (
     <tr onClick={props.onClick}>
       <th scope="row">{props.num}</th>
-      <td>{props.carbon_cost}</td>
-      <td>{props.total_dollars}</td>
-      <td>{props.total_time}</td>
+      <td>{parseFloat(props.carbon_cost).toFixed(2)} kgOfCarbon</td>
+      <td>${parseFloat(props.total_dollars).toFixed(2)}</td>
+      <td>{parseFloat(props.total_time).toFixed(2)} Hours</td>
       <td>{props.tripLabel}</td>
     </tr>
   );
